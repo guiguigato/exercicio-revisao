06f7fea071c81a38d2866c52653cf5ba1d486614
guilherme <58228779+guiguigato@users.noreply.github.com>
2020-10-28 01:59:12 -0300

    exercicio4

---
+++ exercícios/function-area-of-circle.js
@@ -0,0 +1,7 @@
+const  PI  =  Math . PI ;
+function  areacirculo ( Raio )  {
+   área  var =  PI * Raio ** 2 ;
+  console . log ( "A área do círculo é:" ,  área ) ;
+}
+deixe  raio  =  10 ;
+areacirculo ( raio ) ;
